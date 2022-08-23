46b23c0bcc352e3c58e0fe4763cc995280378bd5
Timmy Hanke <timmyhanke@gmail.com>
2022-08-23 20:36:50 +0200

    foodServer

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -23,30 +23,3 @@ export function getChangeFood(id, data) {
 export function deleteFood(id) {
   return http.delete(config.apiEndpointFoods + id);
 }
-
-// export function getFood(id) {
-//   return foods.find((food) => food._id === id);
-// }
-
-// export function saveFood(food) {
-//   let foodInDb = foods.find((f) => f._id === food._id) || {};
-//   foodInDb.name = food.name;
-//   foodInDb.category = getCategories().find(
-//     (category) => category._id === food.categoryId
-//   );
-//   foodInDb.numberInStock = food.numberInStock;
-//   foodInDb.price = food.price;
-
-//   if (!foodInDb._id) {
-//     foodInDb._id = Date.now().toString();
-//     foods.push(foodInDb);
-//   }
-
-//   return foodInDb;
-// }
-
-// export function deleteFood(id) {
-//   let foodInDb = foods.find((food) => food._id === id);
-//   foods.splice(foods.indexOf(foodInDb), 1);
-//   return foodInDb;
-// }
